7dd910e99f2310db6dcb006f0fde110908d33129
Stef <steffelix@hotmail.com>
2025-06-03 13:01:29 +0200

    added final functionality

--- scripts.js
+++ scripts.js
@@ -1,23 +1,61 @@
 
 const container = document.querySelector("div")
+const squares = [];
 
 const squarecontainer = document.createElement("div");
+const button = document.createElement("button");
+
+button.addEventListener("click", () => {
+    var userInput = prompt("Please provide the grid row size:");
+    generateSquares(userInput)
+})
+
+container.appendChild(button);
 container.appendChild(squarecontainer);
+
+button.textContent = "Grid Size";
+button.style.padding = "10px";
+button.style.margin = "0";
+button.style.margin = "30px";
 
 squarecontainer.style.width = "800px";
 squarecontainer.style.display = "flex";
 squarecontainer.style.flexWrap = "wrap";
 
-
 container.style.display = "flex";
+container.style.flexDirection = "column";
 container.style.justifyContent = "center";
+container.style.alignItems = "center"
 
-for(let i = 0; i < 16; i++){
-    const square = document.createElement("div");
+function generateSquares(size){
 
-    square.style.height = "200px";
-    square.style.width = "200px";
-    square.style.background = "red";
+    let totalSquares = size*size;
+    let squareSize = 800 / size;
 
-    squarecontainer.appendChild(square);
-};
+    const newSquares = [];
+    squarecontainer.replaceChildren();
+
+    for(let i = 0; i < size*size; i++){
+        const square = document.createElement("div");
+    
+        square.style.height = `${squareSize}px`;
+        square.style.width = `${squareSize}px`;
+        square.style.background = "red";
+    
+        squarecontainer.appendChild(square);
+    
+        square.addEventListener("mouseover", () => {
+            square.style.background = "blue"
+        })
+    
+        square.addEventListener("mouseout", () => {
+            square.style.background = "red"
+        })
+    
+        squares[i] = square;
+    };
+    
+}
+
+
+
